0aa9ac1db27151a4ced679a3641d1c61fe8a8b0c
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-06-04 17:41:25 -0500

    hashtable notes

--- basic_hashtable/b_hashtables.c
+++ basic_hashtable/b_hashtables.c
@@ -2,6 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Hash tables notes:
+
+// What are has tables?
+// Data structure behind:
+// 	- Associative Arrays and Dictionaries
+// 	- Objects (Python, Javascript, Ruby)
+// 	- Caches (Memcached)
+// 	- Dynamic Programming, Memoization 
+
+// How does a hash table work?
+// 	- Key/Value data storage and retrieval
+// 	- Data is stored in contiguous block of memory as an array
+// 	- The key is converted to an integer via a has function
+// 	- The hashed key is converted to an array index via modulo function
+
+// Hash table is:
+// hash_table[Key] = Value;
+
+// // Pseudocode
+// // int index = hash_function(Key) % array.length;
+// // array[index] = Value;
+
+// What is a Hash Function?
+// - One-way mapping from arbitrary data to fixed data size and type
+
+// - Many different has functions with different attributes:
+// 	- Deterministic
+// 	- Uniform Distribution
+// 	- Non-invertible
+// 	- Continuous vs. non-continuous 
+
+// Hash function:
+
+// // Sums the ascii values of the characters in a string and returns a modulo
+// // String -> integer(0, 31)
+
+// int basic_string_hash(char str[]) {
+// {
+// 	int hash = 0;
+// 	for (int i = 0; str[i] != '\0'; i++) {
+// 	hash += str[i];
+// }
+// return has % 32;
+// }
+
+// basic_string_hash("Hello");
+// // ['H', 'e', 'l', 'l', 'o'] -> {72, 101, 108, 111] -> 500
+// // 500 % 32 = 20
+
+// basic_string_hash("World");
+// // 'W', 'o', 'r', 'l', 'd'] -> [87, 111, 114, 108, 100] -> 520
+// // 520 % 32 = 8
+
+// How do hash tables work?
+
+// 1. Declare an empty array
+// 	- Power of 2 (32)
+// 	- arr = malloc (DEFAULT_SIZE * sizeof (VALUE_TYPE));
+// 2. Hash the key
+// 	- basic_string_hash("Hello') - >20
+// 3. Assign Value to hash index
+// 	- arr[20] = Value;
+
+// What are hash table collisions?
+// 	- Two keys has to the same index
+// 	- How can we avoid collisions?
+// 	- What are the chances of a collision?
+
+// Look up the Birthday Problem on wiki.
+// - Collision Probability = 1 - k! / ((k-n)! * k ^n)
+// - Takeaway: Collisions are unavoidable
+
+// Handling Collisions:
+// There are many different methods:
+// 	- Open addressing
+// 	- Linked list chaining 
+
+// Linked List Chaining:
+// 	- Elements in the has table are stored as linked lists
+// 	- When retrieving a value, traverse down the linked list until you find the 	matching key
+
+// Linke list chaining performance:
+// - Time complexity
+// - Insert, delete, and search for linked lists are O(n) 
+// - Space complexity
+// 	- Must store key, value and pointer for each hash table
+// 	- Empty slots	
+// 	- Still O(n)
+
+// Hash Table Resizing 
+// - Load factor = (number of entires) / (hash table capacity)
+// - When load factor passes a certain threshold, resizing can occur
+// 	- Create a new has table with double capacity
+// 	- Copy elements from the old hash table to the new one. One at a time.
+// 	- Resizing is O(n) BUT occurs at O(log n) frequency 
+
 
 /****
   Basic hash table key/value pair
